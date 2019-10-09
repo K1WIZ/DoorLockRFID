ac1b3c16544e299047f0e10ed2305db179d78d09
John Rogers <k1wiz@wizworks.net>
2019-10-08 23:03:53 -0400

    Create fobcheck.php

---
+++ fobcheck.php
@@ -0,0 +1,47 @@
+<?php
+
+$search = $_POST['search'];
+// DB Credentials
+$servername = "127.0.0.1";
+$username = "";
+$password = "";
+$dbname = "";
+
+// MQTT Credentials
+$pubHost = "";
+$pubUser = "";
+$pubPass = "";
+$topic = "domoticz/in";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$q1    = "SELECT active FROM tags WHERE tag = '$search' LIMIT 1;";
+$result = mysqli_query($conn, $q1);
+
+if (!$result) {
+    echo "DB Error, could not query the database\n";
+    echo 'MySQL Error: ' . mysqli_error();
+    exit;
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    foreach ($row as $field)
+    if ( $field == '1' ) {
+    echo shell_exec(`mosquitto_pub -h $pubHost -u $pubUser -P $pubPass -t $topic -m "{\"command\":\"switchlight\",\"idx\":123,\"switchcmd\":\"Off\"}"`);
+    echo "SPANK";
+    } elseif ( $field == '0' ) { 
+    echo "INVALID";
+    } elseif ( $field == ' ' ) {
+    echo "INVALID";
+    }
+}
+
+mysqli_free_result($result);
+
+?>
